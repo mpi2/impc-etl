43c05895282da888d5646f6f30ead217a90fd33f
ficolo <fico89@gmail.com>
2018-10-31 11:06:09 +0000

    Moving load_tsv from jobs/imits_loader to shared/utils module

--- impc_etl/jobs/loaders/static_data_loader.py
+++ impc_etl/jobs/loaders/static_data_loader.py
@@ -1,5 +1,17 @@
 """
 Static Data loader
     load_human_gene_orthologues:
-    load_phenotyping_centers:
+    load_phenotyping_centres:
 """
+from pyspark.sql import DataFrame, SparkSession
+from impc_etl.shared import utils
+
+
+def load_phenotyping_centres(spark_session: SparkSession, file_path: str) -> DataFrame:
+    """
+    :param spark_session:
+    :param file_path:
+    :return:
+    """
+    phenotyping_centres_df = utils.load_tsv(spark_session, file_path)
+    return phenotyping_centres_df
