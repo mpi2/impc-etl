07064ab630bfe4b6b6478f85539484c7cc7df407
ficolo <fico89@gmail.com>
2018-11-06 15:31:02 +0000

    Static files extraction implementation closes #12, closes #13, closes #21

--- impc_etl/shared/utils.py
+++ impc_etl/shared/utils.py
@@ -2,18 +2,21 @@
 Utils package
 """
 from pyspark.sql import DataFrame, SparkSession, Row
+from pyspark.sql.types import StructType
 from collections import OrderedDict
 
 
-def load_tsv(spark_session: SparkSession, file_path: str) -> DataFrame:
+def load_tsv(spark_session: SparkSession, file_path: str, schema: StructType = None) -> DataFrame:
     """
 
     :param spark_session:
     :param file_path:
+    :param schema:
     :return:
     """
     return spark_session.read.csv(file_path,
                                   header=True,
+                                  schema=schema,
                                   mode='DROPMALFORMED',
                                   sep='\t').load(file_path)
 
