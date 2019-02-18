6aa54c9d38e11de5d11359e2937e5476a95db80f
mrelac <mrelac@ebi.ac.uk>
2019-02-18 19:21:47 +0000

    First cut at working flattened procedures (experiments and lines).

--- impc_etl/jobs/extraction/dcc_schemas.py
+++ impc_etl/jobs/extraction/dcc_schemas.py
@@ -56,25 +56,25 @@ specimenField = [
     StructField('parentalStrain', StructType(parentalStrainField), True),
     StructField('statusCode', StructType(statusCodeField), True)]
 
-centreField = [StructField('_centreID', StringType(), True),
-               StructField('embryo', ArrayType(StructType(specimenField)), True),
-               StructField('mouse', ArrayType(StructType(specimenField)), True),
-               StructField('ns2:embryo', ArrayType(StructType(specimenField)), True),
-               StructField('ns2:mouse', ArrayType(StructType(specimenField)), True)]
+centreSpecimenField = [StructField('_centreID', StringType(), True),
+                       StructField('embryo', ArrayType(StructType(specimenField)), True),
+                       StructField('mouse', ArrayType(StructType(specimenField)), True),
+                       StructField('ns2:embryo', ArrayType(StructType(specimenField)), True),
+                       StructField('ns2:mouse', ArrayType(StructType(specimenField)), True)]
 
 
-def get_specimen_centre_schema():
-    return StructType(centreField)
+def get_centre_specimen_schema():
+    return StructType(centreSpecimenField)
 
 
 def flatten_specimen_df(centre_df: DataFrame,
                         source_file: str,
                         datasource_short_name: str) -> DataFrame:
     """
-    Flattens out the centreID, mouse, and embryo specimen data and adds type ('mouse' or 'embryo'),
+    Flattens out the specimen centreID, mouse, and embryo specimen data and adds type ('mouse' or 'embryo'),
     source file, and datasourceShortName (e.g. IMPC, 3i), returning the flattened DataFrame.
 
-    :param centre_df: The hierarchical DataFrame containing centreID and optional sub components 'mouse' and 'embryo'
+    :param centre_df: The hierarchical specimen DataFrame containing centreID and sub components 'mouse' and 'embryo'
     :param source_file: The xml source file name
     :param datasource_short_name: The data source (e.g. IMPC, 3i)
     :return: A dataframe containing, at the top level:
@@ -98,8 +98,6 @@ def flatten_specimen_df(centre_df: DataFrame,
 
     mice_df = centre_df.select(centre_df['_centreID'], centre_df[mouse_col_name])
     embryos_df = centre_df.select(centre_df['_centreID'], centre_df[embryo_col_name])
-    # mice_df = centre_df.select(centre_df['_centreID'], centre_df.mouse)
-    # embryos_df = centre_df.select(centre_df['_centreID'], centre_df.embryo)
 
     exploded_mice = mice_df\
         .withColumn('tmp', explode_outer(mice_df[mouse_col_name])).select('tmp.*', '_centreID')\
@@ -110,6 +108,175 @@ def flatten_specimen_df(centre_df: DataFrame,
         .withColumn('_type', lit('embryo'))
 
     df = exploded_mice.union(exploded_embryos)\
+        .withColumn('_sourceFile', lit(source_file)) \
+        .withColumn('_datasourceShortName', lit(datasource_short_name))
+
+    return df
+
+
+# procedure_definition.xsd    NOTE: Last parameter of StructField is 'isNullable'
+
+dimensionField = [StructField('_id', StringType(), True),
+                  StructField('_origin', StringType(), True),
+                  StructField('_unit', StringType(), True)]
+
+parameterAssociationField = [StructField('_dim', ArrayType(StructType(dimensionField)), True),
+                             StructField('_parameterID', StringType(), True),
+                             StructField('_sequenceID', IntegerType(), True)]
+
+procedureMetadataField = [StructField('_parameterStatus', StringType(), True),
+                          StructField('_value', StringType(), True),
+                          StructField('_parameterID', StringType(), True),
+                          StructField('_sequenceID', IntegerType(), True)]
+
+seriesMediaParameterValueField =\
+    [StructField('parameterAssociation', ArrayType(StructType(parameterAssociationField)), True),
+     StructField('procedureMetadata', ArrayType(StructType(procedureMetadataField)), True),
+     StructField('_incrementValue', StringType(), True),
+     StructField('_URI', StringType(), True),
+     StructField('_fileType', StringType(), True)]
+
+seriesMediaParameterField =\
+    [StructField('value', ArrayType(StructType(seriesMediaParameterValueField)), True),
+     StructField('_parameterStatus', StringType(), True),
+     StructField('procedureMetadata', ArrayType(StructType(procedureMetadataField)), True),
+     StructField('_parameterID', StringType(), True)]
+
+mediaFileField = [StructField('procedureMetadata', ArrayType(StructType(procedureMetadataField)), True),
+                  StructField('parameterAssociation', ArrayType(StructType(parameterAssociationField)), True),
+                  StructField('_localId', StringType(), True),
+                  StructField('_URI', StringType(), True),
+                  StructField('_fileType', StringType(), True)]
+
+mediaSectionField = [StructField('mediaFile', ArrayType(StructType(mediaFileField)), True),
+                     StructField('_localId', StringType(), True)]
+
+mediaSampleField = [StructField('mediaSection', ArrayType(StructType(mediaSectionField)), True),
+                    StructField('_localId', StringType(), True)]
+
+mediaSampleParameterField =\
+    [StructField('mediaSample', ArrayType(StructType(mediaSampleField)), True),
+     StructField('_parameterStatus', StringType(), True),
+     StructField('_parameterID', StringType(), True)]
+
+mediaParameterField = [StructField('_parameterStatus', StringType(), True),
+                       StructField('parameterAssociation', ArrayType(StructType(parameterAssociationField)), True),
+                       StructField('procedureMetadata', ArrayType(StructType(procedureMetadataField)), True),
+                       StructField('_parameterID', StringType(), True),
+                       StructField('_URI', StringType(), True),
+                       StructField('_fileType', StringType(), True)]
+
+seriesParameterValueField = [StructField('_incrementValue', StringType(), True),
+                             StructField('_incrementStatus', StringType(), True)]
+
+seriesParameterField = [StructField('value', ArrayType(StructType(seriesParameterValueField)), True),
+                        StructField('_parameterStatus', StringType(), True),
+                        StructField('_parameterID', StringType(), True),
+                        StructField('_sequenceID', IntegerType(), True),
+                        StructField('_unit', StringType(), True)]
+
+ontologyParameterField = [StructField('_term', ArrayType(StringType()), True),
+                          StructField('_parameterStatus', StringType(), True),
+                          StructField('_parameterID', StringType(), True),
+                          StructField('_sequenceID', IntegerType(), True)]
+
+simpleParameterField = [StructField('_value', StringType(), True),
+                        StructField('_parameterStatus', StringType(), True),
+                        StructField('_parameterID', StringType(), True),
+                        StructField('_unit', StringType(), True),
+                        StructField('_sequenceID', IntegerType(), True)]
+
+procedureField = [StructField('simpleParameter', ArrayType(StructType(simpleParameterField)), True),
+                  StructField('ontologyParameter', ArrayType(StructType(ontologyParameterField)), True),
+                  StructField('seriesParameter', ArrayType(StructType(seriesParameterField)), True),
+                  StructField('mediaParameter', ArrayType(StructType(mediaParameterField)), True),
+                  StructField('mediaSampleParameter', ArrayType(StructType(mediaSampleParameterField)), True),
+                  StructField('seriesMediaParameter', ArrayType(StructType(seriesMediaParameterField)), True),
+                  StructField('procedureMetadata', ArrayType(StructType(procedureMetadataField)), True),
+                  StructField('_procedureID', StringType(), True)]
+
+# The fields in experimentField and lineField must match exactly in name, position, and type, as they are joined.
+# The fields not native to each field type have the 'Copied from ...' comment.
+experimentField = [
+    StructField('_specimenID', ArrayType(StringType()), True),
+    StructField('_colonyID', StringType(), True),               # Copied from lineField.
+    StructField('_sequenceID', StringType(), True),
+    StructField('_experimentID', StringType(), True),
+    StructField('_dateOfExperiment', StringType(), True),
+    StructField('procedure', StructType(procedureField), True),
+    StructField('statusCode', ArrayType(StructType(statusCodeField)), True)
+
+    # These fields exist in line but not in experiment. They are added here to make the line/experiment join compatible.
+]
+lineField = [
+    StructField('_specimenID', ArrayType(StringType()), True),
+    StructField('_colonyID', StringType(), True),
+    StructField('_sequenceID', StringType(), True),             # Copied from experimentField.
+    StructField('_experimentID', StringType(), True),           # Copied from experimentField.
+    StructField('_dateOfExperiment', StringType(), True),       # Copied from experimentField.
+    StructField('procedure', StructType(procedureField), True),
+    StructField('statusCode', ArrayType(StructType(statusCodeField)), True)
+]
+
+housingField = [StructField('procedure', StructType(procedureField), True),
+                StructField('_fromLIMS', BooleanType(), True),
+                StructField('_lastUpdated', StringType(), True)]
+
+centreProcedureField = [StructField('experiment', ArrayType(StructType(experimentField)), True),
+                        StructField('line', ArrayType(StructType(lineField)), True),
+                        StructField('housing', ArrayType(StructType(housingField)), True),
+                        StructField('_centreID', StringType(), True),
+                        StructField('_pipeline', StringType(), True),
+                        StructField('_project', StringType(), True)]
+
+
+def get_centre_procedure_schema():
+    return StructType(centreProcedureField)
+
+
+def flatten_procedure_df(centre_df: DataFrame,
+                         source_file: str,
+                         datasource_short_name: str) -> DataFrame:
+    """
+    Flattens out the procedure centreID, line and experiment data and adds type ('line' or 'experiment'),
+    source file, and datasourceShortName (e.g. IMPC, 3i), returning the flattened DataFrame.
+
+    :param centre_df: The hierarchical procedure DataFrame containing centreID and optional sub components 'mouse' and 'embryo'
+    :param source_file: The xml source file name
+    :param datasource_short_name: The data source (e.g. IMPC, 3i)
+    :return: A dataframe containing, at the top level:
+        _centreID
+        _type ('line' or 'experiment')
+        _sourceFile (the xml source file)
+        _datasourceShortName (the data source (e.g. IMPC, 3i)
+        the flattened contents of the 'line' and 'experiment' records
+
+    """
+    df: DataFrame
+
+    if centre_df is None:
+        return df
+
+    # FIXME FIXME FIXME - No hack for experiments!
+    # HACK for Spark Xml's inability to handle NAMESPACES!
+    # mouse_col_name = 'mouse' if datasource_short_name != '3i' else 'ns2:mouse'
+    # embryo_col_name = 'embryo' if datasource_short_name != '3i' else 'ns2:embryo'
+
+    experiment_df = centre_df.na.drop(subset=['line']).select(centre_df['_centreID'], centre_df['experiment'])
+    line_df = centre_df.na.drop(subset=['experiment']).select(centre_df['_centreID'], centre_df['line'])
+
+    # experiment_df = centre_df.filter("centre_df.line is None").select(centre_df['_centreID'], centre_df['experiment'])
+    # line_df = centre_df.select(centre_df['_centreID'], centre_df['line']).filter(centre_df['experiment'] is None)
+
+    exploded_experiments = experiment_df\
+        .withColumn('tmp', explode_outer(experiment_df['experiment'])).select('tmp.*', '_centreID')\
+        .withColumn('_type', lit('experiment'))
+
+    exploded_lines = line_df\
+        .withColumn('tmp', explode_outer(line_df['line'])).select('tmp.*', '_centreID')\
+        .withColumn('_type', lit('line'))
+
+    df = exploded_experiments.union(exploded_lines)\
         .withColumn('_sourceFile', lit(source_file)) \
         .withColumn('_datasourceShortName', lit(datasource_short_name))
 
