776ff2be6662779d5f25a0200bb961f9ca4f4c48
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-05-27 18:20:00 +0100

    Adding ontology terms accs to Pipeline parquet

--- impc_etl/jobs/load/solr/pipeline_mapper.py
+++ impc_etl/jobs/load/solr/pipeline_mapper.py
@@ -7,6 +7,7 @@ import sys
 
 
 COLUMN_MAPPER = {
+    "schedule_key": "scheduleId",
     "stage": "schedule.stage",
     "stage_label": "schedule.timeLabel",
     "pipeline_id": "pipelineId",
@@ -27,11 +28,34 @@ COLUMN_MAPPER = {
     "annotate": "parameter.isAnnotation",
     "media": "parameter.isMedia",
     "has_options": "parameter.isOption",
-    "metadata": "",
     "increment": "parameter.isIncrement",
-    "unit_x": "parameter.unit",
-    "observation_type": "",
-    "fully_qualified_name": "",
+}
+
+MISSING = {
+    "unit_y": "if increment != null parameter.unit else null",
+    "unit_x": "if increment != null increment.unit else parameter.unit",
+    "metadata": "parameter.type == 'procedureMetadata'",
+    "observation_type": "first observations.observation_type where fully_qualified_name",
+    "fully_qualified_name": "concat(pipeline_key, procedure_key, parameter_key)",
+    "categories": "",
+    "mp_id": "",
+    "mp_term": "",
+    "top_level_mp_id": "",
+    "top_level_mp_term": "",
+    "top_level_mp_term_synonym": "",
+    "intermediate_mp_id": "",
+    "intermediate_mp_term": "",
+    "abnormal_mp_id": "",
+    "abnormal_mp_term": "",
+    "decreased_mp_id": "",
+    "decreased_mp_term": "",
+    "increased_mp_id": "",
+    "increased_mp_term": "",
+    "anatomy_id": "",
+    "anatomy_term": "",
+    "emap_id": "",
+    "ma_id": "",
+    "ma_term": "",
 }
 
 
