8aa85ea0d77d63a2e3d659b313ba5250f21f552c
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-08-27 13:53:51 +0100

    Change raw data storage to just one field with all the data inside of it as a JSON dict

--- impc_etl/jobs/load/solr/stats_results_mapper.py
+++ impc_etl/jobs/load/solr/stats_results_mapper.py
@@ -33,6 +33,7 @@ from pyspark.sql.functions import (
     array_repeat,
     to_json,
 )
+import json
 from pyspark.sql.types import (
     StructType,
     StructField,
@@ -711,7 +712,7 @@ def _parse_raw_data(open_stats_df):
             "category",
         ),
     )
-    open_stats_df = open_stats_df.withColumn("raw_data", to_json("raw_data_struct"))
+    open_stats_df = open_stats_df.withColumn("raw_data", udf(lambda d: json.dumps(d)))
     open_stats_df.select("raw_data").show(10, truncate=False)
     raise ValueError
     open_stats_df = open_stats_df.withColumn(
