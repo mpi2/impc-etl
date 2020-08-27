4c9dff8a36cd6cde3f1589d8d5f0004230391239
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-08-27 13:30:23 +0100

    Change raw data storage to just one field with all the data inside of it as a JSON dict

--- impc_etl/jobs/load/solr/stats_results_mapper.py
+++ impc_etl/jobs/load/solr/stats_results_mapper.py
@@ -711,6 +711,7 @@ def _parse_raw_data(open_stats_df):
             "category",
         ),
     )
+    open_stats_df.select("raw_data").show(10, truncate=False)
     open_stats_df = open_stats_df.withColumn("raw_data", to_json("raw_data"))
     open_stats_df.select("raw_data").show(10, truncate=False)
     raise ValueError
