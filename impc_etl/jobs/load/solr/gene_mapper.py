b0072b7bb519c1e5c2dd67ce44ce19a6a531efee
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-10-02 12:31:33 +0100

    Add raw_data to gene core

--- impc_etl/jobs/load/solr/gene_mapper.py
+++ impc_etl/jobs/load/solr/gene_mapper.py
@@ -71,8 +71,6 @@ GENE_CORE_COLUMNS = [
     "ensembl_gene_id",
     "ccds_id",
     "ncbi_id",
-    "significant_top_level_mp_terms",
-    "not_significant_top_level_mp_terms",
 ]
 
 IMITS_GENE_COLUMNS = [
