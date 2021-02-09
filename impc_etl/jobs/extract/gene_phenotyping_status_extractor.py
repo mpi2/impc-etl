5eb9ebdd67879db6df508de56aa8cce0afff283e
Federico Lopez Gomez <federico@ebi.ac.uk>
2021-02-09 18:44:52 +0000

    Adding mapping to the gene_phenotyping_status_extractor

--- impc_etl/jobs/extract/gene_phenotyping_status_extractor.py
+++ impc_etl/jobs/extract/gene_phenotyping_status_extractor.py
@@ -1,7 +1,8 @@
 import luigi
 from luigi.contrib.spark import PySparkTask
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import when, col, lit
+from pyspark.sql.functions import when, col, lit, lower, udf
+from pyspark.sql.types import StringType, IntegerType
 
 from impc_etl.workflow.config import ImpcConfig
 
@@ -29,7 +30,6 @@ class GenePhenotypingStatusExtractor(PySparkTask):
             new_col_name = (
                 "imits_" + col_name.replace("gene_", "")
                 if "mgi_accession_id" not in col_name
-                and "marker_symbol" not in col_name
                 else col_name.replace("gene_", "")
             )
             imits_gene_status_df = imits_gene_status_df.withColumnRenamed(
@@ -46,23 +46,19 @@ class GenePhenotypingStatusExtractor(PySparkTask):
         }
         for col_name in gentar_gene_status_df.columns:
             new_col_name = (
-                "gentar_" + col_name
-                if "mgi_accession_id" not in col_name
-                and "marker_symbol" not in col_name
-                else col_name
+                "gentar_" + col_name if "mgi_accession_id" not in col_name else col_name
             )
             gentar_gene_status_df = gentar_gene_status_df.withColumnRenamed(
                 col_name, new_col_name
             )
         gene_status_df = imits_gene_status_df.join(
             gentar_gene_status_df,
-            ["mgi_accession_id", "marker_symbol"],
+            "mgi_accession_id",
             "full",
         )
 
         gene_statuses_cols = [
             "mgi_accession_id",
-            "marker_symbol",
             "assignment_status",
             "null_allele_production_status",
             "conditional_allele_production_status",
@@ -92,41 +88,39 @@ class GenePhenotypingStatusExtractor(PySparkTask):
 
         gene_status_df = gene_status_df.withColumn(
             "crispr_allele_production_status",
-            col("gentar_conditional_allele_production_status"),
-        )
-        gene_status_df = gene_status_df.withColumn(
-            "project_status",
-            col("gentar_conditional_allele_production_status"),
+            col("gentar_crispr_allele_production_status"),
         )
 
-        ALLELE_MOUSE_PROD_STATUS_MAP = {
+        allele_mouse_prod_status_map = {
             "Chimeras obtained": "Assigned for Mouse Production and Phenotyping",
+            "Micro-injection in progress": "Assigned for Mouse Production and Phenotyping",
             "Cre Excision Complete": "Mice Produced",
             "Cre Excision Started": "Mice Produced",
-            "ES Cell Production in Progress": "Assigned for ES Cell Production",
-            "ES Cell Targeting Confirmed": "ES Cells Produced",
             "Genotype confirmed": "Mice Produced",
-            "Micro-injection in progress": "Assigned for Mouse Production and Phenotyping",
-            "No ES Cell Production": "Not Assigned for ES Cell Production",
             "Phenotype Attempt Registered": "Mice Produced",
             "Rederivation Complete": "Mice Produced",
             "Rederivation Started": "Mice Produced",
         }
 
-        # status_map_df_json = spark.sparkContext.parallelize(
-        #     [
-        #         {"allele_production_status": key, "mice_production_status": value}
-        #         for key, value in ALLELE_MOUSE_PROD_STATUS_MAP.items()
-        #     ]
-        # )
-        # status_map_df = spark.read.json(status_map_df_json)
-        #
-        # for status_col in [
-        #     "null_allele_production_status",
-        #     "conditional_allele_production_status",
-        # ]:
-        #     gene_status_df = gene_status_df.withColumn(status_col)
+        gene_status_df = self.map_status(
+            spark,
+            gene_status_df,
+            allele_mouse_prod_status_map,
+            "mouse_production_status",
+        )
 
+        allele_es_cells_prod_status_map = {
+            "No ES Cell Production": "Not Assigned for ES Cell Production",
+            "ES Cell Production in Progress": "Assigned for ES Cell Production",
+            "ES Cell Targeting Confirmed": "ES Cells Produced",
+        }
+
+        gene_status_df = self.map_status(
+            spark,
+            gene_status_df,
+            allele_es_cells_prod_status_map,
+            "es_cell_production_status",
+        )
         gene_status_df.printSchema()
         gene_status_df.select(gene_statuses_cols).show(truncate=False)
 
@@ -178,4 +172,48 @@ class GenePhenotypingStatusExtractor(PySparkTask):
             )
             .otherwise(lit("data_issue")),
         )
+        return gene_status_df
+
+    def map_status(self, spark, gene_status_df, status_map_dict, target_status_col):
+        status_map_df_json = spark.sparkContext.parallelize(
+            [
+                {
+                    "src_production_status": key,
+                    "dst_production_status": value,
+                }
+                for key, value in status_map_dict.items()
+            ]
+        )
+        status_map_df = spark.read.json(status_map_df_json)
+        gene_status_df.withColumn(target_status_col, lit(None).astype(StringType()))
+        get_status_hierarchy_udf = udf(
+            lambda x: list(status_map_dict.values()).index(x),
+            IntegerType(),
+        )
+
+        for status_col in [
+            "null_allele_production_status",
+            "conditional_allele_production_status",
+            "crispr_allele_production_status",
+        ]:
+            gene_status_df = gene_status_df.join(
+                status_map_df,
+                lower(col(status_col)) == lower(col("src_production_status")),
+                "left_outer",
+            )
+            gene_status_df.withColumn(
+                target_status_col,
+                when(
+                    col(target_status_col).isNull()
+                    & col("dst_production_status").isNotNull(),
+                    col("dst_production_status"),
+                )
+                .when(
+                    get_status_hierarchy_udf("dst_production_status")
+                    > get_status_hierarchy_udf("mouse_production_status"),
+                    col("dst_production_status"),
+                )
+                .otherwise(col(target_status_col)),
+            )
+            gene_status_df.drop(["src_production_status", "dst_production_status"])
         return gene_status_df
