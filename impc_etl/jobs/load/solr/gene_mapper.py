b59d4b56e23667d870d9ca103cbf9084be3b361d
Federico Lopez Gomez <federico@ebi.ac.uk>
2021-02-12 14:16:40 +0000

    Change mapping source for ES Cell production status

--- impc_etl/jobs/load/solr/gene_mapper.py
+++ impc_etl/jobs/load/solr/gene_mapper.py
@@ -253,9 +253,6 @@ def main(argv):
     )
     gene_df = gene_df.join(mgi_datasets_df, "mgi_accession_id", "left_outer")
     gene_df = gene_df.join(significant_mp_term, "mgi_accession_id", "left_outer")
-    gene_df.printSchema()
-    gene_df.select(*GENE_CORE_COLUMNS).distinct().show()
-    raise TypeError
     gene_df.select(*GENE_CORE_COLUMNS).distinct().write.parquet(output_path)
 
 
