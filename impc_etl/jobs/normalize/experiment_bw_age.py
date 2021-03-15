b09b9ec5ea40725323f816bce4f5a1f0687a4125
Federico Lopez Gomez <federico@ebi.ac.uk>
2021-03-15 11:04:18 +0000

    Add logic to manage fasted weight parameters

--- impc_etl/jobs/normalize/experiment_bw_age.py
+++ impc_etl/jobs/normalize/experiment_bw_age.py
@@ -172,15 +172,15 @@ def get_associated_body_weight(
         not_use_body_weight_parameters,
         (
             (
-                dcc_experiment_df["experiment._pipeline"]
+                dcc_experiment_df["exp._pipeline"]
                 == not_use_body_weight_parameters["pipelineKey"]
             )
             & (
-                dcc_experiment_df["experiment._procedureID"]
+                dcc_experiment_df["exp._procedureID"]
                 == not_use_body_weight_parameters["procedureKey"]
             )
             & (
-                dcc_experiment_df["experiment.simpleParameter._parameterID"]
+                dcc_experiment_df["exp.simpleParameter._parameterID"]
                 == not_use_body_weight_parameters["parameterKey"]
             )
         ),
