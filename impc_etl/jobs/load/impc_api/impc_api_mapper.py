6548f3fb48eed1b70a3e98214563879b48d6600d
Federico Lopez Gomez <federico@ebi.ac.uk>
2021-06-04 06:45:06 +0100

    Fixing dependency management

--- impc_etl/jobs/load/impc_api/impc_api_mapper.py
+++ impc_etl/jobs/load/impc_api/impc_api_mapper.py
@@ -124,14 +124,14 @@ class ApiObservationMapper(ApiMapper):
         "time_point",
         "discrete_point",
         "increment_value",
-        "subterm_id",
+        "sub_term_id",
         "sub_term_name",
         "sub_term_description",
     ]
     column_renaming = {
         "sequence_id": "parameter_sequence_id",
-        "subterm_id": "ontology_term_id",
-        "subterm_name": "ontology_term_name",
+        "sub_term_id": "ontology_term_id",
+        "sub_term_name": "ontology_term_name",
         "sub_term_description": "ontology_term_description",
     }
     extra_transformations = [
