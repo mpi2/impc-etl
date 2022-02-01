3fbaeb59b257cf754ddb37d822298dd71bb90f99
Federico López Gómez <fico89@gmail.com>
2022-02-01 12:33:58 +0000

    Finish Experiment cleaner restructure

--- impc_etl/workflow/cleaning.py
+++ impc_etl/workflow/cleaning.py
@@ -1,42 +1,13 @@
+from impc_etl.jobs.clean.experiment_cleaner import IMPCExperimentCleaner
 from impc_etl.workflow.extraction import *
 
 
-class ExperimentCleaner(SparkSubmitTask):
-    name = "Experiment_Cleaner"
-    app = "impc_etl/jobs/clean/experiment_cleaner.py"
-    output_path = luigi.Parameter()
-    entity_type = "experiment"
-    dcc_xml_path = luigi.Parameter()
-    resources = {"overwrite_resource": 1}
-
-    def requires(self):
-        return SpecimenExperimentExtractor()
-
-    def output(self):
-        output_path = self.input().path.replace("_raw", "")
-        return ImpcConfig().get_target(output_path)
-
-    def app_options(self):
-        return [self.input().path, self.entity_type, self.output().path]
+class SpecimenExperimentCleaner(IMPCExperimentCleaner):
+    experiment_type = "specimen_level"
 
 
-class LineExperimentCleaner(SparkSubmitTask):
-    name = "Line_Experiment_Cleaner"
-    app = "impc_etl/jobs/clean/experiment_cleaner.py"
-    output_path = luigi.Parameter()
-    entity_type = "line"
-    dcc_xml_path = luigi.Parameter()
-    resources = {"overwrite_resource": 1}
-
-    def requires(self):
-        return LineExperimentExtractor()
-
-    def output(self):
-        output_path = self.input().path.replace("_raw", "")
-        return ImpcConfig().get_target(output_path)
-
-    def app_options(self):
-        return [self.input().path, self.entity_type, self.output().path]
+class LineExperimentCleaner(IMPCExperimentCleaner):
+    experiment_type = "line_level"
 
 
 class MouseCleaner(SparkSubmitTask):
