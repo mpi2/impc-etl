faeab98626e110bc21b33ab0583739142ba27149
Federico López Gómez <federico@ebi.ac.uk>
2022-03-07 12:59:51 +0000

    Adding StatisticalAnalysisOutputMapper to main module

--- impc_etl/workflow/main.py
+++ impc_etl/workflow/main.py
@@ -2,6 +2,7 @@ import luigi
 
 from impc_etl.jobs.load.impc_images_mapper import ImagesPipelineInputGenerator
 from impc_etl.jobs.load.stats_pipeline_input_mapper import StatsPipelineInputMapper
+from impc_etl.jobs.extract.open_stats_extractor import StatisticalAnalysisOutputMapper
 
 
 class ImpcPreStatisticalAnalysis(luigi.Task):
