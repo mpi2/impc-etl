a536550736294c9455a071c8c783bc988bf1d355
Federico López Gómez <federico@ebi.ac.uk>
2022-03-19 15:21:42 +0000

    Test PWG load

--- impc_etl/jobs/load/solr/stats_results_mapper.py
+++ impc_etl/jobs/load/solr/stats_results_mapper.py
@@ -327,14 +327,7 @@ class StatsResultsMapper(PySparkTask):
             STATS_OBSERVATIONS_JOIN + ["datasource_name", "production_center"]
         ).agg(*aggregation_expresion)
 
-        print(
-            "After map to stats observations_metadata_df IMPC_FOR_001_001 H-GRIA1-DEL588-EM1-B6N obs"
-        )
-        observations_metadata_df.where(
-            f.col("parameter_stable_id") == "IMPC_FOR_001_001"
-        ).where(f.col("colony_id") == "H-GRIA1-DEL588-EM1-B6N").show(
-            vertical=True, truncate=False
-        )
+        # REMOVE after DR
         pwg_metadata_df = observations_metadata_df.where(
             f.col("datasource_name") == "pwg"
         ).withColumnRenamed("metadata_group", "pwg_metadata_group")
@@ -362,13 +355,6 @@ class StatsResultsMapper(PySparkTask):
             OBSERVATIONS_STATS_MAP,
             "observation",
         )
-        print(
-            "After map to stats observations_metadata_df IMPC_FOR_001_001 H-GRIA1-DEL588-EM1-B6N open"
-        )
-        open_stats_df.where(f.col("parameter_stable_id") == "IMPC_FOR_001_001").where(
-            f.col("colony_id") == "H-GRIA1-DEL588-EM1-B6N"
-        ).show(vertical=True, truncate=False)
-        raise ValueError
 
         open_stats_df = open_stats_df.withColumn(
             "pipeline_stable_id",
