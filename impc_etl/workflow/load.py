2962292f320e4470b151d61ffceafa6f5d7565ce
Federico López Gómez <federico@ebi.ac.uk>
2022-03-25 16:42:24 +0000

    Update impc_etl/workflow/load.py to use newer parquet2solr jar

--- impc_etl/workflow/load.py
+++ impc_etl/workflow/load.py
@@ -7,7 +7,7 @@ from impc_etl.workflow.normalization import *
 
 
 class Parquet2Solr(SparkSubmitTask):
-    app = "lib/parquet2solr-0710-2021.jar"
+    app = "lib/parquet2solr-2503-2022.jar"
     name = "IMPC_Parquet2Solr"
     input_path = luigi.Parameter()
     output_path = luigi.Parameter()
