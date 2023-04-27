b6fc95b4febab57e89cfc3bf26374afb70b56c3f
Tudor Groza <tudor.groza@gmail.com>
2023-04-27 13:41:00 +0800

    Testing removal of derivation.

--- impc_etl/jobs/transform/experiment_parameter_derivator.py
+++ impc_etl/jobs/transform/experiment_parameter_derivator.py
@@ -148,20 +148,14 @@ class ParameterDerivator(PySparkTask):
             dataCollect = experiment_df.rdd.toLocalIterator()
             europhenomeList = []
             euroPhenomeHashes = {}
-            restHashes = {}
 
             for row in dataCollect:
                 rowHash = self.computeHash(row)
                 if not rowHash:
-                    #                    europhenomeList.append(row)
                     continue
 
                 if rowHash in hashedData:
-                    count = 0
-                    if rowHash in euroPhenomeHashes:
-                        count = euroPhenomeHashes[rowHash]
-                    count += 1
-                    euroPhenomeHashes[rowHash] = count
+                    euroPhenomeHashes[rowHash] = ''
 
                     dataDict = row.asDict()
                     europhenomeData = hashedData[rowHash]
@@ -177,31 +171,33 @@ class ParameterDerivator(PySparkTask):
                     dataDict['simpleParameter'] = euroList
                     newRow = Row(**dataDict)
                     europhenomeList.append(newRow)
-                else:
-                    count = 0
-                    if rowHash in restHashes:
-                        count = restHashes[rowHash]
-                    count += 1
-                    restHashes[rowHash] = count
-            #                    europhenomeList.append(row)
 
-            count = 0
+            print('Removing from main DF ... ')
+            count = 1
             for rowHash in euroPhenomeHashes:
-                if rowHash in restHashes:
-                    count += 1
-            print(' - Duplicate hashes across: {}'.format(count))
-            count = 0
-            for rowHash in euroPhenomeHashes:
-                if euroPhenomeHashes[rowHash] != 1:
-                    count += 1
-            print(' - Duplicate hashes inside: {}'.format(count))
+                print(' - Count: {}'.format(count))
+                segs = rowHash.split('#')
+                experiment_df = experiment_df.filter(
+                    (
+                            (experiment_df['_pipeline'] != segs[0]) &
+                            (experiment_df['_procedureID'] != segs[1]) &
+                            (experiment_df['_project'] != segs[2]) &
+                            (experiment_df['_sequenceID'] != segs[3]) &
+                            (experiment_df['specimenID'] != segs[4]) &
+                            (experiment_df['_experimentID'] != segs[5]) &
+                            (experiment_df['_dataSource'] != segs[6]) &
+                            (experiment_df['_centreID'] != segs[7])
+                    )
+                )
+                count += 1
 
-            print(' -- BEFORE: {}'.format(len(europhenomeList)))
-            europhenome_df = spark.createDataFrame(europhenomeList, schema=_schema)
-            print('EUROPHENOME after [COUNT]: {}'.format(europhenome_df.count()))
-            print('EUROPHENOME after [COUNT]: {}'.format(len(europhenome_df.columns)))
+        print('Experiment after [COUNT]: {}'.format(experiment_df.count()))
+        print('Experiment after [COLS]: {}'.format(len(experiment_df.columns)))
+        europhenome_df = spark.createDataFrame(europhenomeList, schema=_schema)
+        print('EUROPHENOME after [COUNT]: {}'.format(europhenome_df.count()))
+        print('EUROPHENOME after [COUNT]: {}'.format(len(europhenome_df.columns)))
 
-            europhenome_df.write.parquet(output_path)
+        europhenome_df.write.parquet(output_path)
 
 
 class SpecimenLevelExperimentParameterDerivator(ParameterDerivator):
