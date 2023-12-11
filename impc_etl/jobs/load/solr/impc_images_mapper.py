1574dfa27b26fc34dc722fd236f40321c5aaa8a0
Federico López Gómez <federico@ebi.ac.uk>
2023-12-11 17:54:40 +0000

    Adding media info file type checking for PDF files

--- impc_etl/jobs/load/solr/impc_images_mapper.py
+++ impc_etl/jobs/load/solr/impc_images_mapper.py
@@ -17,6 +17,7 @@ from pyspark.sql.functions import (
     explode_outer,
     concat,
     lit,
+    regexp_extract,
 )
 
 from impc_etl.jobs.load import ExperimentToObservationMapper
@@ -28,6 +29,7 @@ class ImpcImagesLoader(PySparkTask):
     name = "IMPC_Images_Core_Loader"
     omero_ids_csv_path = luigi.Parameter()
     output_path = luigi.Parameter()
+    imaging_media_json_info_path = luigi.Parameter()
 
     def requires(self):
         return [
@@ -40,6 +42,7 @@ class ImpcImagesLoader(PySparkTask):
             self.input()[0].path,
             self.input()[1].path,
             self.omero_ids_csv_path,
+            self.imaging_media_json_info_path,
             self.output().path,
         ]
 
@@ -61,7 +64,8 @@ class ImpcImagesLoader(PySparkTask):
         observations_parquet_path = args[0]
         pipeline_core_parquet_path = args[1]
         omero_ids_csv_path = args[2]
-        output_path = args[3]
+        imaging_media_json_info_path = args[3]
+        output_path = args[4]
 
         spark = SparkSession.builder.getOrCreate()
         observations_df = spark.read.parquet(observations_parquet_path)
@@ -84,6 +88,17 @@ class ImpcImagesLoader(PySparkTask):
             col("intermediate_mp_term").alias("impress_intermediate_mp_term"),
         ).distinct()
         omero_ids_df = spark.read.csv(omero_ids_csv_path, header=True).dropDuplicates()
+        media_info_df = (
+            spark.read.json(imaging_media_json_info_path, multiLine=True)
+            .dropDuplicates()
+            .select("checksum", "fileName")
+        )
+        media_info_df = media_info_df.withColumn(
+            "isPdf",
+            when(col("fileName").contains(".pdf"), lit(True)).otherwise(lit(False)),
+        )
+        media_info_df = media_info_df.drop("fileName")
+
         omero_ids_df = omero_ids_df.alias("omero")
         image_observations_df = observations_df.where(
             col("observation_type") == "image_record"
@@ -102,6 +117,19 @@ class ImpcImagesLoader(PySparkTask):
             ],
         )
         image_observations_df = image_observations_df.select("obs.*", "omero.omero_id")
+        image_observations_df = image_observations_df.withColumn(
+            "checksum",
+            regexp_extract("download_file_path", r"\/.*\/(.+)", 1),
+        )
+        image_observations_df = image_observations_df.join(media_info_df, "checksum")
+        image_observations_df = image_observations_df.withColumn(
+            "file_type",
+            when(
+                (col("isPdf") == True) & (col("file_type").isNull()),
+                lit("application/pdf"),
+            ).otherwise(col("file_type")),
+        )
+        image_observations_df = image_observations_df.drop("checksum", "isPdf")
         image_observations_exp_df = image_observations_df.withColumn(
             "parameter_association_stable_id_exp",
             explode_outer("parameter_association_stable_id"),
