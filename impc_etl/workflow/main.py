61fdf01aeb6e1ce3ccbdc6547733c4ed5bbdd061
Federico López Gómez <federico@ebi.ac.uk>
2024-01-02 15:33:16 +0000

    Adding mapper for GWAS catalog external links service

--- impc_etl/workflow/main.py
+++ impc_etl/workflow/main.py
@@ -24,6 +24,7 @@ from impc_etl.jobs.load.impc_api.impc_api_mapper import (
     ImpcPhenotypeStatisticalResultsMapper,
     ImpcImagesMapper,
     ImpcBWTDatasetsMapper,
+    ImpcExternalLinksMapper,
 )
 from impc_etl.jobs.load.impc_api.impc_bulk_api_mapper import ImpcBulkApiMapper
 from impc_etl.jobs.load.impc_api.impc_bulk_api_mongo_load import ImpcBulkApiMongoLoader
@@ -187,4 +188,5 @@ class ImpcWebApiMapper(luigi.Task):
             ImpcPhenotypeStatisticalResultsMapper(),
             ImpcImagesMapper(),
             ImpcBWTDatasetsMapper(),
+            ImpcExternalLinksMapper(),
         ]
