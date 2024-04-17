4fe9abe62975663cf33b8e547860302d0b2e620d
Federico López Gómez <federico@ebi.ac.uk>
2024-04-17 15:08:32 +0100

    Change index_directory script to manage port

--- lib/index_directory.sh
+++ lib/index_directory.sh
@@ -14,13 +14,16 @@ DIRECTORY="$4"
 CORE_NAME="$5"
 
 # Start Solr
-sh "$SOLR_SCRIPTS_PATH/start.sh $SOLR_PORT"
+echo "Starting Solr in port $SOLR_PORT"
+bash "$SOLR_SCRIPTS_PATH/start.sh" $SOLR_PORT
+wait
+sleep 60
 
 # Delete all the documents on the Solr Core
-eval "curl -X POST -H 'Content-Type: application/json' 'http://localhost:$SOLR_PORT/$CORE_NAME/update' --data-binary '{\"delete\": {\"query\":\"*:*\"}, \"commit\": {}}'"
+echo "Deleting all documents Solr in port under http://localhost:$SOLR_PORT/solr/$CORE_NAME/"
+eval "curl -X POST -H 'Content-Type: application/json' 'http://localhost:$SOLR_PORT/solr/$CORE_NAME/update' --data-binary '{\"delete\": {\"query\":\"*:*\"}, \"commit\": {}}'"
+wait
 
-# Execute the command to delete all documents
-eval "$DELETE_COMMAND"
 
 # Define the command to execute for each file
 COMMAND="$SOLR_POST_PATH -p $SOLR_PORT -commit false -c $CORE_NAME"
@@ -39,8 +42,9 @@ export -f process_file
 # Change to the directory
 cd "$DIRECTORY" || exit 1
 
+pwd
 # Split the list of files in the directory into groups of 10 files each
-find . -maxdepth 1 -type f -name 'part*.json' | split -l 10 - file_list_part_
+find . -maxdepth 1 -type f -name 'part*.json.gz' | split -l 10 - file_list_part_
 
 # Process each group of files in parallel
 for file_group in file_list_part_*; do
@@ -55,6 +59,6 @@ eval "curl http://localhost:$SOLR_PORT/solr/$CORE_NAME/update -F stream.body='<o
 rm file_list_part_*
 
 # Stop Solr
-sh "$SOLR_SCRIPTS_PATH/stop.sh"
+bash "$SOLR_SCRIPTS_PATH/stop.sh" $SOLR_PORT
 touch "$DIRECTORY/_INDEX_SUCCESS"
 exit  0
